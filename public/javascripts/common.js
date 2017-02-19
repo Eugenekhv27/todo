55fcf74d892bac00228a2e7073435991fb7c6ed7
Khveugene27 <evnikolaevv@gmail.com>
2017-02-19 15:40:02 +1000

    button add 100% ( Front)

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -2,11 +2,7 @@ var model = {
 
 	items: [
 
-	{date: "25.02.17", name: "Покупки", count: 15},
-	{date: "23.02.17", name: "Список дел", count: 3},
-	{date: "22.02.17", name: "Покупки", count: 8},
-	{date: "19.02.17", name: "Сегодня", count: 13},
-	{date: "15.02.17", name: "Позвонить", count: 6}
+	
 
 	]
 }
@@ -17,7 +13,51 @@ var purchaseApp = angular.module("purchaseApp", []);
     purchaseApp.controller("purchaseController", function ($scope) {
     $scope.list = model;
 	$scope.addItem = function () {
-		var elem = angular.element(document.querySelector(".addnewmainlist"));
-		console.log(document.getElementsByClassName('addnewmainlist')[0].style.display = 'block');
+		document.getElementsByClassName('addnewmainlist')[0].style.display = 'block';		
+		document.getElementsByClassName('addnewmainlist')[0].style.opacity = '1';
+		
+		
+		
+    }
+
+    $scope.SetNewItem = function(Iname){
+    	console.log(Iname);
+    	var datep = new Date()
+    	if(String(datep.getMonth()+1).length = 1)
+    	{
+    		month = '0' + String(datep.getMonth()+1)
+    	}else{
+
+    		month = String(datep.getMonth()+1);
+    	}
+
+    	var datenow = String(datep.getDate()) + '.' + month + '.' + String(datep.getFullYear())[2] + String(datep.getFullYear())[3] ;
+    	$scope.list.items.push({ date: datenow, name: Iname, count: 0 });
+    	document.getElementsByClassName('addnewmainlist')[0].style.transition = '0.5s';
+    	document.getElementsByClassName('addnewmainlist')[0].style.opacity = '0.1';
+    	function setDisplay(){
+    		document.getElementsByClassName('addnewmainlist')[0].style.display = 'none';
+    		document.getElementsByClassName('add-input')[0].value = ' ';
+    		
+    	}
+    	setTimeout(setDisplay, 500);
+
+    	
+    }
+
+    $scope.SetNewCancel = function(){
+    	document.getElementsByClassName('addnewmainlist')[0].style.transition = '0.5s';
+    	document.getElementsByClassName('addnewmainlist')[0].style.opacity = '0.1';
+    	function setDisplay(){
+    		document.getElementsByClassName('addnewmainlist')[0].style.display = 'none';
+    	}
+    	setTimeout(setDisplay, 500);
+
+
+
+
     }
 });
+
+
+
