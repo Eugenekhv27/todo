f0ad1fdaed1a784a8cff290ae6d08c3c101266fe
Eugene <evnikolaevv@gmail.com>
2017-02-20 13:06:56 +1000

    All works without delete function and DB

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -1,12 +1,12 @@
 var model = {
 
-    ids:2,
+    ids:0,
 
 	items: [
-
+/*
     {id: 0,date: "19.02.2017",name:"Позвонить",does:[{check: 'false', name: "Вася",time:"13:08"},{check: 'true', name: "Петя",time:"13:07"},{check: 'true', name: "Марина",time:"13:07"}]},
-    {id: 1,date: "19.02.2017",name:"Дела",does:[{check: 'false', name: "Пописать",time:"13:07"},{check: 'false', name: "Покурить",time:"13:07"},{check: 'false', name: "Посрать",time:"13:07"}]},
-    {id: 2,date: "19.02.2017",name:"Список продуктов",does:[{check: 'false', name: "Хлеб",time:"13:07"},{check: 'false', name: "Молоко",time:"13:07"}]}
+    {id: 1,date: "19.02.2017",name:"Дела",does:[{check: 'false', name: "Приготовить",time:"13:07"},{check: 'false', name: "К стоматологу в час ",time:"13:07"},{check: 'true', name: "Настроить компьютер",time:"13:07"},{check: 'false', name: "На пары",time:"13:07"}]},*/
+    {id: 0,date: " ",name:"Основные задачи",does:[{check: 'false', name: "Проснутся",time:"13:07"}]}
 
     ]
 
@@ -26,9 +26,26 @@ var purchaseApp = angular.module("purchaseApp", []);
 		
     }
 
-    $scope.OpenDo = function(x){
-        alert($scope.list.items[0].id)
+    $scope.openAddDo = function () {
+        document.getElementsByClassName('does-add-row')[0].style.display = 'block';
+        document.getElementsByClassName('does-add-row')[0].style.opacity = '1';
+        document.getElementsByClassName('does-add-row')[0].style.value = ' ';       
     }
+
+    
+
+    ;
+    $scope.getTime = function(){
+        datee = new Date();
+        var minut = '';
+        if(String(datee.getMinutes()).lenght == 1)
+        {
+           minut = '0' + String(datee.getMinutes())
+        }else{
+            minut = String(datee.getMinutes())
+        }
+        return String(datee.getHours()) + ':' + minut 
+    } 
 
     $scope.SetNewItem = function(Iname){
     	
@@ -42,7 +59,8 @@ var purchaseApp = angular.module("purchaseApp", []);
     	}
 
     	var datenow = String(datep.getDate()) + '.' + month + '.' + String(datep.getFullYear())[2] + String(datep.getFullYear())[3] ;
-    	$scope.list.items.push({ id: $scope.list.ids+1, date: datenow, name: Iname, does:[{check: 'false', name: "Хлеб"}]});
+    	$scope.list.items.push({ id: $scope.list.ids+1, date: datenow, name: Iname, does:[{check : 'true', name : 'Для удаления этоя записи, добавьте новую', time : '00:00'}]});
+        $scope.list.ids++;
     	document.getElementsByClassName('addnewmainlist')[0].style.transition = '0.5s';
     	document.getElementsByClassName('addnewmainlist')[0].style.opacity = '0.1';
     	function setDisplay(){
@@ -64,13 +82,48 @@ var purchaseApp = angular.module("purchaseApp", []);
     	setTimeout(setDisplay, 500);
     }
 
+    
+    $scope.setNewDo = function(iden,doName){
+        for (var i = 0; i < $scope.list.items.length; i++) {
+
+            if($scope.list.items[i].id == iden){
+                
+                $scope.list.items[i].does.push({check: 'false', name: doName,time:$scope.getTime()})
+                console.log($scope.list.items[i].does)
+                $scope.getDoes(iden);
+                document.getElementsByClassName('does-add-row')[0].style.transition = '0.5s';   
+                document.getElementsByClassName('does-add-row')[0].style.opacity = '0.1';
+                function setDisplay(){
+                    document.getElementsByClassName('does-add-row')[0].style.display = 'none';       
+                }       
+                setTimeout(setDisplay,500)
+                
+
+                break;
+            }
+            
+        }
+    }
+
     $scope.getDoes = function(x){
+        
+        $scope.Doid = {
+            elem: [{id : 0}]
+        };
+        if(x == undefined || x==NaN || x == '' || x == 0)
+        {
+            $scope.Doid.elem[0].id= 0
+
+        }else{
+          $scope.Doid.elem[0].id= x
+
+        }
         var doModel = {
             items:[]
         }
 
         $scope.currentDo = doModel;
-        if(x == undefined || x==NaN || x == '' || x == 0)
+        if(x==undefined || x==NaN || x == '' || x == 0)
         {
             for (var i = 0 ; i < $scope.list.items[0].does.length; i++) {
                 $scope.currentDo.items.push({check : $scope.list.items[0].does[i].check, name : $scope.list.items[0].does[i].name, time : $scope.list.items[0].does[i].time})
@@ -80,12 +133,12 @@ var purchaseApp = angular.module("purchaseApp", []);
             
         }else{
             for (var i = 0 ; i < $scope.list.items.length; i++) {
-                console.log($scope.list.items[i].id)
+                
                 if( $scope.list.items[i].id == x)
                 {
                     for (var j = 0; j < $scope.list.items[i].does.length; j++) {
                         $scope.currentDo.items.push({check : $scope.list.items[i].does[j].check, name : $scope.list.items[i].does[j].name, time : $scope.list.items[i].does[j].time})
-                            console.log($scope.list.items[i].does[j].check)
+                            $scope.list.items[i].does[j].check
                     };
                     break;
                     
